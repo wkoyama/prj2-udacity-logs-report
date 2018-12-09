39e9580872f5ec5dd6f90af48a325d0e74424837
Wesley Kenji <wkenji.oyama@gmail.com>
2018-12-09 03:32:03 -0200

    ajustes de queries

--- queries de projeto de log.sql
+++ queries de projeto de log.sql
@@ -2,7 +2,6 @@
 select a.title, count(l.*) as view
 from articles a, log l 
 where l.path like '%' || a.slug
-and l.status like '200%'
 group by a.slug, a.title
 order by view desc
 limit 3;
@@ -11,7 +10,6 @@ limit 3;
 select aut.name, count(l.*) as view
 from articles a, log l, authors aut
 where a.author = aut.id and l.path like '%' || a.slug
-and l.status like '200%'
 group by aut.name, a.slug
 order by view desc
 limit 3;
@@ -25,7 +23,7 @@ from log lg, (
         count(l.status) as total,
         date_trunc('day', l.time) as date
         from log l
-        group by date_trunc('day', l.time), l.status
+        group by date_trunc('day', l.time)
         order by date_trunc('day', l.time) desc ) Total
 where lg.status not like '200%' and Total.date = date_trunc('day', lg.time)
 group by date_trunc('day', lg.time), status, Total.total
